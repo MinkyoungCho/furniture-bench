d164a1f653e02db509413128e293d54e56648188
Joey Brewington <brewing@umich.edu>
2025-11-24 20:42:57 -0500

    Move objects to center of table

--- furniture_bench/envs/furniture_sim_env.py
+++ furniture_bench/envs/furniture_sim_env.py
@@ -568,6 +568,7 @@ class FurnitureSimEnv(gym.Env):
         else:
             pos = part.reset_pos[self.from_skill]
             ori = part.reset_ori[self.from_skill]
+        pos[0] += 0.2
         return pos, ori
 
     def set_viewer(self):
